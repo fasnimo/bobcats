f68496a5038e3037269d4986e69554485ef7a7c4
fasnimo <fasnimo@gmail.com>
2020-02-19 13:42:51 -0600

    Files have Error

--- code-challenges/boat-rentals/app/controllers/application_controller.rb
+++ code-challenges/boat-rentals/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def date_time
+    boat.update_at.strftime("Last Update, %A, %b, %e, at %1:M %p")
+  end 
 end
